68d590fefbf428dfff88cb96f01619e451ec9d75
KKKHun2 <1224kim2son@naver.com>
2023-06-25 21:54:38 +0900

    ⚡책리스트 페이지 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,52 @@
 import Seo from "../components/Seo";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
-import { AiOutlineArrowRight } from "react-icons/ai";
 import Link from "next/link";
 
 interface IMovieProps {
-    list_name: string,
-    display_name: string,
-    list_name_encoded: string,
-    rank:number,
+  list_name: string;
+  display_name: string;
+  list_name_encoded: string;
+  rank: number;
 }
 
-function Home({ results }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    const router = useRouter();
-    
-    return (
-    <div className="container"
-       >
+function Home({
+  results,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+
+  return (
+    <div className="container">
       <Seo title="Home" />
       {results?.map((results: IMovieProps) => (
-       <div
-       className="book">
-        <Link href={`/list/${results.list_name}`}>
-        <h4>{results.list_name} &rarr; </h4></Link>
+        <div className="book">
+          <Link href={`/list/${results.list_name}`}>
+            <h4>{results.list_name} &rarr; </h4>
+          </Link>
         </div>
       ))}
       <style jsx>{`
         .container {
           display: flex;
-          padding: 20px;
-          gap: 20px;
+          padding: 10px;
+          gap: 10px;
           max-width: 100%;
           justify-content: center;
           flex-wrap: wrap;
-          color:black;
-          padding:10px;
+          color: black;
+          padding: 10px;
         }
         .book {
           cursor: pointer;
           align-items: center;
           justify-content: center;
           flex-direction: row;
-          margin: 20px;
-          box-sizing:border-box;
-          padding: 40px;
-          border: solid rgba(47, 54, 64,1.0) 7px;
-          box-sizing:border-box;;
-          border-radius:15px;
+          margin: 10px;
+          box-sizing: border-box;
+          padding: 20px;
+          border: solid rgba(47, 54, 64, 1) 7px;
+          box-sizing: border-box;
+          border-radius: 15px;
         }
         .book:hover {
           transform: scale(1.05) translateY(-10px);
@@ -54,21 +54,19 @@ function Home({ results }: InferGetServerSidePropsType<typeof getServerSideProps
         .book h4 {
           font-size: 17px;
           text-align: center;
-          color:black;
-          padding:8px;
+          color: black;
+          padding: 8px;
         }
       `}</style>
     </div>
   );
-  ``;
 }
-
-
 
 export async function getServerSideProps({}: GetServerSideProps) {
   const { results } = await (
-    await fetch(`http://localhost:3000/api/books`)).json();
-    console.log(results)
+    await fetch(`http://localhost:3000/api/books`)
+  ).json();
+  console.log(results);
   return {
     props: {
       results,
--- pages/list/[id].tsx
+++ pages/list/[id].tsx
@@ -1,108 +1,113 @@
 import Seo from "../../components/Seo";
-import {  GetServerSideProps, InferGetServerSidePropsType, GetServerSidePropsContext } from "next";
-// import {useRouter} from "next/router";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  GetServerSidePropsContext,
+} from "next";
+import React from "react";
 
+interface Detail {
+  list_name: string;
+  books: Book[];
+}
 
-export default function Detail(
-    {books,}:InferGetServerSidePropsType<typeof getServerSideProps>){
+interface Book {
+  title: string;
+  author: string;
+  book_image: string;
+  amazon_product_url: string;
+}
 
-    return (
-        <div>
+export const getServerSideProps: GetServerSideProps<{
+  books: Detail;
+}> = async ({ params }: GetServerSidePropsContext) => {
+  const response = await fetch(`http://localhost:3000/api/books/${params?.id}`);
+  const json = await response.json();
+  return { props: { books: json.results } };
+};
+
+export default function Detail({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
       <Seo title={books.list_name} />
       <div className="wrapper">
         <span className="title">{books.list_name}</span>
       </div>
       <div className="container">
-        {books.books.map(book => (
-          <div 
-          key={book.amazon_product_url}
-          className="bookList">
+        {books.books.map((book) => (
+          <div className="bookList">
             <img
-            className="img"
+              className="img"
               src={book.book_image}
-              width={300}
-              height={390}
+              width={200}
+              height={260}
             />
-            <h3>{book.title.length <19 ? book.title : book.title.slice(0,18)+ "..."}</h3>
-            <p >{book.author}</p>
-            <a href={book.amazon_product_url}>
-              Buy now &rarr;
-            </a>
+            <h3>
+              {book.title.length < 19
+                ? book.title
+                : book.title.slice(0, 18) + "..."}
+            </h3>
+            <p>{book.author}</p>
+            <a href={book.amazon_product_url}>Buy now &rarr;</a>
           </div>
         ))}
-        </div>
-        <style jsx>{`
-        .wrapper{
-          font-size:30px;
+      </div>
+      <style jsx>{`
+        .wrapper {
+          font-size: 30px;
           padding: 25px;
           gap: 20px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          align-content: center;
         }
         .container {
-          display:grid;
+          display: grid;
+          width:95vw;
           grid-template-columns: repeat(4, 1fr);
           padding: 25px;
-          gap: 20px;
+          gap: 10px;
           justify-items: center;
-          align-items: center;
-        
         }
-        .title{
-          margin:30px;
-          width:10px;
-          height:10px;
-          box-sizing:border-box;
-          border: solid rgba(47, 54, 64,1.0) 2px;
+        .title {
+          margin: 30px;
+          box-sizing: border-box;
+          border: solid rgba(47, 54, 64, 1) 2px;
+          background-color:rgba(113, 128, 147,1.0);
+          border-radius: 10px;
         }
-        .bookList{
-          border: solid rgba(47, 54, 64,1.0) 7px;
-          box-sizing:border-box;
+        .bookList {
+          border: solid rgba(47, 54, 64, 1) 7px;
+          box-sizing: border-box;
           padding: 20px;
-        box-shadow: 12px 12px 2px 1px rgba(47, 54, 64,1.0);
-        margin:10px;
-        background:#f8f8f8;
-        cursor:pointer;
+          box-shadow: 12px 12px 2px 1px rgba(47, 54, 64, 1);
+          margin: 20px;
+          background: #f8f8f8;
+          cursor: pointer;
         }
         .bookList:hover {
           transform: scale(1.05) translateY(-10px);
         }
-        .bookList img{
-          border-radius:10px;
+        .bookList img {
+          border-radius: 10px;
         }
-        .bookList h3{
-          margin-top:15px;
-          font-size:25px;
+        .bookList h3 {
+          margin-top: 15px;
+          font-size: 15px;
         }
-        .bookList p{
-          margin-top:-20px;
-          font-size:15px;
+        .bookList p {
+          margin-top: -13px;
+          font-size: 13px;
         }
-        .bookList a{
-          margin-top:25px;
-          font-size:20px;
-          color:black;
+        .bookList a {
+          margin-top: 25px;
+          font-size: 20px;
+          color: black;
         }
       `}</style>
-        </div>
-    )
-        }
-interface Detail{
-  list_name: string,
-  books: Book[],
-};
-
-interface Book {
-    title: string,
-    author: string,
-    book_image: string,
-    amazon_product_url: string,
+   </>
+  );
 }
-
-
-
-export const getServerSideProps: GetServerSideProps<{
-    books: Detail;
-  }> = async ({ params }: GetServerSidePropsContext) => {
-    const res = await fetch(`http://localhost:3000/api/books/${params?.id}`);
-    const json = await res.json();
-    return { props: { books: json.results } };
-  };
